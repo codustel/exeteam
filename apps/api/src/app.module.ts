4309ab86724439f8120b45e51f5e7b5031b79a08
codustel <ismael.belaaouad@codustel.fr>
2026-02-28 19:09:48 +0100

    feat(api): add CustomFieldsModule with config/data CRUD and validation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,8 +5,13 @@ import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { SitesModule } from './sites/sites.module';
 import { ClientsModule } from './clients/clients.module';
+import { SitesModule } from './sites/sites.module';
+import { EmployeesModule } from './employees/employees.module';
+import { LeavesModule } from './leaves/leaves.module';
+import { PublicHolidaysModule } from './public-holidays/public-holidays.module';
+import { CodesProduitsModule } from './codes-produits/codes-produits.module';
+import { CustomFieldsModule } from './custom-fields/custom-fields.module';
 
 @Module({
   imports: [
@@ -23,8 +28,13 @@ import { ClientsModule } from './clients/clients.module';
     PrismaModule,
     AuthModule,
     UsersModule,
-    SitesModule,
     ClientsModule,
+    SitesModule,
+    EmployeesModule,
+    LeavesModule,
+    PublicHolidaysModule,
+    CodesProduitsModule,
+    CustomFieldsModule,
   ],
   controllers: [AppController],
 })
